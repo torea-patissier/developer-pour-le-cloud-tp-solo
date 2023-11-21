48462f3c178058d8fb19bab8d16e2ba9daf231f4
Torea Patissier <p.torea@icloud.com>
2023-11-21 10:02:02 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
       <div className='flex h-screen w-screen justify-center items-center text-3xl font-bold'>
-      <h1> Salut gros bg !!!!</h1>
+      <h1> Salut gros bg !!!</h1>
       </div>
   )
 }
